4296d5cac8774d57c58b24da73e69b46ace07c27
Dima Babenko <klapster1@gmail.com>
2025-11-22 10:27:25 +0300

    Aded brackets

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
-    private final Random random = new Random;
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
